44e27a3ade7c3ef48ac64563f12562daccb7185d
Konrad Breipohl <konrad.breipohl@uni-bielefeld.de>
2023-10-16 21:44:04 +0200

    test

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ release = '0.1'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+#exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
