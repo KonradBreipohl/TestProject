ae56225369dbf82b3baec67c233ea9c9a73cdb5d
Konrad Breipohl <konrad.breipohl@uni-bielefeld.de>
2023-10-16 11:05:39 +0200

    test

--- conf.py
+++ conf.py
@@ -4,6 +4,13 @@ author = 'testAuthor'
 release = '0.1'
 version = '0.1.0'
 
+import subprocess
+
+# Install 'sphinx_rtd_theme' during the build on Read the Docs
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+if read_the_docs_build:
+    subprocess.call('pip install sphinx_rtd_theme', shell=True)
+
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
