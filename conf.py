6acec2b3033a1d21f9f40286e6bda15839a7908d
Konrad Breipohl <konrad.breipohl@uni-bielefeld.de>
2023-10-16 10:59:49 +0200

    test

--- conf.py
+++ conf.py
@@ -4,6 +4,20 @@ author = 'testAuthor'
 release = '0.1'
 version = '0.1.0'
 
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
 
 # -- Options for HTML output
